b47d816bbe03f1fa0e8e8bf900f2df727fdca2e9
kevinn-h <kevinh449@nycstudents.net>
2026-09-15 09:20:21 -0400

    our second commit

--- app.py
+++ app.py
@@ -4,6 +4,25 @@ t = Turtle()
 
 t.shape('turtle')
 
+""" t.forward(200)
+t.left(90)
 t.forward(200)
-t.up (200)
+t.left(90)
+t.forward(200)
+t.left(90)
+t.forward(200)
+ """
+""" t.forward(200)
+t.left (120)
+t.forward(200)
+t.left(120)
+t.forward(200)
+ """
+t.forward(200)
+t.left(90)
+t.forward(200)
+t.left(135)
+t.forward(285)
 turtle.done()
+
+
---
+++ loops.py
@@ -0,0 +1,9 @@
+import turtle
+from turtle import
+t = Turtle()
+
+for i in range(4)
+t.forward(200)
+t.left(90)
+
+turtle.done()
