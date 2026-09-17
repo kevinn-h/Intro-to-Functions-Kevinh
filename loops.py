da0d4dbcbc61f742efd7bcbb0e887a02b14c3ffb
kevinn-h <kevinh449@nycstudents.net>
2026-09-17 09:20:14 -0400

    4th commit

--- loops.py
+++ loops.py
@@ -2,19 +2,29 @@ import turtle
 from turtle import *
 t = Turtle()
 t.speed(1000)
-
+   
+""" def square(x,y):
+    t.forward(x)
+    t.left(y)
+    t.forward(x)
+    t.left(y)
+    t.forward(x)
+    t.left(y)
+    t.forward(x)
+    t.left(y)
 for i in range(100):
-    def square(x):
-        t.forward(x)
-        t.left(90)
-        t.forward(x)
-        t.left(90)
-        t.forward(x)
-        t.left(90)
-        t.forward(x)
-        t.left(90)
-    square(200)
+    square(200,90)   
     t.left(5)
 
-turtle.done()
+turtle.done() 
 
+ """
+
+
+def doubleSquares(iRange):
+    length = 5
+    for i in range (iRange):
+    square(length, 90)
+    length = length * 1.15
+    t.right(5)
+doubleSquares(60)
